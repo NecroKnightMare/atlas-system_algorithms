f228aca36bf07ae228fa23bfc5e10c14e77a1284
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-20 20:48:29 -0500

    files ready, file 0-2. need to fill readme, added graph display

---
+++ graphs/0-graph_create.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "graphs.h"
+
+/**
+ * graph_create - Creates a new graph
+ * 
+ * Return: Pointer to the newly created graph, or NULL on failure
+ */
+
+graph_t *graph_create(void)
+{
+	graph_t *new_graph;
+
+	new_graph = malloc(sizeof(graph_t));
+
+	if(!new_graph)
+		return (NULL);
+
+	new_graph->vertices = NULL;
+	new_graph->nb_vertices = 0;
+
+	return (new_graph);
+}
---
+++ graphs/1-graph_add_vertex.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <string.h>
+#include "graphs.h"
+
+/**
+ * graph_add_vertex - Adds a new vertex to the graph
+ * @graph: Pointer to graph structure
+ * @str: string to store vertex
+ * 
+ * Return: pointer to created vertex, or NULL on failure
+ */
+
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *vertex, *current;
+	char *str_dup;
+
+	if (!graph || !str)
+		return (NULL);
+	current = graph->vertices;
+	while (current)
+	{
+		if (strcmp(current->content, str) == 0)
+			return (NULL); /* San francisco not added */
+		current = current->next;
+	}
+	vertex = malloc(sizeof(vertex_t));
+	if (!vertex)
+		return (NULL);
+	str_dup = strdup(str);
+	if (!str_dup)
+	{
+		free(vertex);
+		return (NULL);
+	}
+	vertex->content = str_dup;
+	vertex->index = graph->nb_vertices;
+	vertex->edges = NULL;
+	vertex->nb_edges = 0;
+	vertex->next = NULL;
+	if (!graph->vertices)
+		graph->vertices = vertex;
+	else
+	{
+		current = graph->vertices;
+		while (current->next)
+			current = current->next;
+		current->next = vertex;
+	}
+	graph->nb_vertices++;
+	return (vertex);
+}
