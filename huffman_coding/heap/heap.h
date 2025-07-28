f1abe30f6ddd44104523e459de3105174db79134
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-07-28 18:39:10 -0500

    added prot to header and 1 main file

--- huffman_coding/heap/heap.h
+++ huffman_coding/heap/heap.h
@@ -33,5 +33,6 @@ typedef struct heap_s
 
 /*prototypes*/
 heap_t *heap_create(int (*data_cmp)(void *, void *));
+binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
 
 #endif /* _HEAP_H_ */
