dc6d7963f47b24d1d1c5003166bd7f5dc5dce296
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-08-05 23:25:42 -0500

    changed spaces to tabs

--- huffman_coding/huffman_tree.c
+++ huffman_coding/huffman_tree.c
@@ -12,54 +12,54 @@
 
 binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size)
 {
-    heap_t *heap;
-    binary_tree_node_t *node1, *node2, *new;
-    symbol_t *new_symbol;
+	heap_t *heap;
+	binary_tree_node_t *node1, *node2, *new;
+	symbol_t *new_symbol;
 
-    if (!data || !freq || size == 0)
-        return (NULL);
+	if (!data || !freq || size == 0)
+		return (NULL);
 
-    heap = huffman_priority_queue(data, freq, size);
-    if (!heap)
-        return (NULL);
+	heap = huffman_priority_queue(data, freq, size);
+	if (!heap)
+		return (NULL);
 
-    while (heap->size > 1)
-    {
-        node1 = heap_extract(heap);
-        node2 = heap_extract(heap);
-        if (!node1 || !node2)
-        {
-            heap_delete(heap, free);
-            return (NULL);
-        }
-        new_symbol = symbol_create(-1, ((symbol_t *)node1->data)->freq +
-                                    ((symbol_t *)node2->data)->freq);
+	while (heap->size > 1)
+	{
+		node1 = heap_extract(heap);
+		node2 = heap_extract(heap);
+		if (!node1 || !node2)
+		{
+			heap_delete(heap, free);
+			return (NULL);
+		}
+		new_symbol = symbol_create(-1, ((symbol_t *)node1->data)->freq +
+					((symbol_t *)node2->data)->freq);
 
-        if (!new_symbol)
-        {
-            heap_delete(heap, free);
-            return (NULL);
-        }
+		if (!new_symbol)
+		{
+			heap_delete(heap, free);
+			return (NULL);
+		}
 
-        new = binary_tree_node(NULL, new_symbol);
-        if (!new)
-        {
-            free(new_symbol);
-            heap_delete(heap, free);
-            return (NULL);
-        }
-        new->left = node1;
-        new->right = node2;
-        node1->parent = new;
-        node2->parent = new;
+		new = binary_tree_node(NULL, new_symbol);
+		if (!new)
+		{
+			free(new_symbol);
+			heap_delete(heap, free);
+			return (NULL);
+		}
+		new->left = node1;
+		new->right = node2;
+		node1->parent = new;
+		node2->parent = new;
 
-        if (!heap_insert(heap, new))
-        {
-            heap_delete(heap, free);
-            return (NULL);
-        }
-    }
-    new = heap_extract(heap);
-    heap_delete(heap, free);
-    return (new);
+		if (!heap_insert(heap, new))
+		{
+			heap_delete(heap, free);
+			return (NULL);
+		}
+	}
+	new = heap_extract(heap);
+	heap_delete(heap, free);
+	return (new);
 }
