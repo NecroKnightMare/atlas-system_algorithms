545086f24d3d9ec1afe66f2e94f81ff3d0b9df9e
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-08-20 10:25:10 -0500

    betty fixes

--- nary_trees/0-nary_tree_insert.c
+++ nary_trees/0-nary_tree_insert.c
@@ -10,33 +10,33 @@
  */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
-    nary_tree_t *new_node;
+	nary_tree_t *new_node;
 
 	if (!str)
 		return (NULL);
 
-    new_node = malloc(sizeof(nary_tree_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(nary_tree_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->content = strdup(str);
-    if (!new_node->content)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->content = strdup(str);
+	if (!new_node->content)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->parent = parent;
-    new_node->nb_children = 0;
-    new_node->children = NULL;
-    new_node->next = NULL;
+	new_node->parent = parent;
+	new_node->nb_children = 0;
+	new_node->children = NULL;
+	new_node->next = NULL;
 
-    if (parent)
-    {
+	if (parent)
+	{
 		new_node->next = parent->children;
 		parent->children = new_node;
-        parent->nb_children++;
-    }
+		parent->nb_children++;
+	}
 
-    return (new_node);
+	return (new_node);
 }
