501d1bc2420c2f4bfb9ecc10d05cc967ce56a1b6
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2025-08-20 10:20:14 -0500

    insert function

---
+++ nary_trees/0-nary_tree_insert.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include "nary_trees.h"
+
+/**
+ * nary_tree_insert - Inserts a new node into an N-ary tree
+ * @parent: Pointer to the parent node
+ * @str: String to be stored in the new node
+ * Return: Pointer to the new node, or NULL
+ */
+nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
+{
+    nary_tree_t *new_node;
+
+	if (!str)
+		return (NULL);
+
+    new_node = malloc(sizeof(nary_tree_t));
+    if (!new_node)
+        return (NULL);
+
+    new_node->content = strdup(str);
+    if (!new_node->content)
+    {
+        free(new_node);
+        return (NULL);
+    }
+
+    new_node->parent = parent;
+    new_node->nb_children = 0;
+    new_node->children = NULL;
+    new_node->next = NULL;
+
+    if (parent)
+    {
+        if (!parent->children)
+            parent->children = new_node;
+        else
+        {
+            nary_tree_t *tmp = parent->children;
+            while (tmp->next)
+                tmp = tmp->next;
+            tmp->next = new_node;
+        }
+        parent->nb_children++;
+    }
+
+    return (new_node);
+}
